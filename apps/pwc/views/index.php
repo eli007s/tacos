2a8918e9fea22ce450befc915b544a1bb9506528
Eli Orellana <eli007s@gmail.com>
2020-03-15 00:28:26 -0700

    🌮

--- apps/pwc/views/index.php
+++ apps/pwc/views/index.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PWC Clean</title>
+    <style>
+        .textarea {
+            width: 400px;
+            height: 500px;
+        }
+    </style>
 </head>
 <body>
     <div id="container">
         <form action="/clean" method="post">
-            <textarea name="text"><?php echo isset($_POST['text']) && strlen($_POST['text']) > 0 ? $_POST['text'] : ''; ?></textarea>
+            <textarea class="textarea" name="text"><?php echo isset($_POST['text']) && strlen($_POST['text']) > 0 ? $_POST['text'] : ''; ?></textarea>
             <input type="submit" name="submit" />
         </form>
     </div>
